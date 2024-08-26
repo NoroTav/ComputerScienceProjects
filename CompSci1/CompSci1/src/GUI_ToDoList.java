a34f1d9d6b33d8e8db9c03e1839761332b82adbe
tav <tavarisct123@gmail.com>
2024-08-26 14:27:38 -0400

    Began Creating the GUI functionality for the todo list app

--- CompSci1/CompSci1/src/GUI_ToDoList.java
+++ CompSci1/CompSci1/src/GUI_ToDoList.java
@@ -4,47 +4,53 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.*;
 import java.awt.GridLayout;
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.jar.JarEntry;
 
-public class GUI  implements ActionListener {
+public class GUI_ToDoList implements ActionListener {
     JLabel label;
     JFrame frame;
     JPanel panel;
     JEditorPane ep;
-    private int count = 0;
 
 //Create the GUI Class and Settings
-    public GUI(){
+    public GUI_ToDoList(ArrayList list){
         frame = new JFrame();
         panel = new JPanel();
-        ep = new JEditorPane();
-        JButton button  = new JButton("Click Me!");
-        label = new JLabel("Number of clicks: 0");
-        ep.setText("Type here");
-        button.addActionListener(this);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
         panel.setLayout(new GridLayout(0,1));
-        panel.add(button, ep);
-        panel.add(label) ;
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        JLabel[] labels = new JLabel[list.size()];
+        for(int i=0; i< list.size();i++)
+        {
+            labels[i] = new JLabel();
+            labels[i].setText("YOU NEED TO: " +"\n"+ list.get(i));
+            labels[i].setBounds(100,50,50,50);
+            panel.add(labels[i]);
+            System.out.println(labels[i]);
+        }
+
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("My Gui");
         frame.pack();
+        frame.setTitle("TODOLISTAPPLICATION");
+        frame.setSize(500,800);
         frame.setVisible(true);
+
+
+
+
     }
 //Main Code (Run the GUI)
     public static void main(String[] args){
-        new GUI();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks: "+ count);
     }
 }
--- CompSci1/CompSci1/src/ToDoApp.java
+++ CompSci1/CompSci1/src/ToDoApp.java
@@ -1,2 +1,47 @@
-public class ToDoApp {
+//To Do app project
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class ToDoApp{
+
+
+
+    static ArrayList<String> doList = new ArrayList<>();
+    public static void main(String[] args)
+    {
+        //Take user input and save it
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello, would you like to create a task?(y/n)");
+        String createList = scan.nextLine();
+
+        if (createList.toLowerCase().equals("y"))
+        {
+            while (true) {
+                System.out.println("What do you want to add?");
+                String todo = scan.nextLine();
+                doList.add(todo);
+
+                System.out.println("Add more?(y/n)");
+                String nextTask = scan.nextLine();
+
+                if (nextTask.toLowerCase().equals("n")) {
+                    System.out.println("Do you want to look at what you have then?(y/n)");
+                    String inList = scan.nextLine();
+                    if (inList.toLowerCase().equals("y")) {
+                        new GUI_ToDoList(doList);
+                        System.out.println(doList);
+                        break;
+                    } else {
+                        System.out.println("Then why open the program...");
+                    }
+                }
+            }
+        }else if(createList.toLowerCase().equals("n"))
+        {
+            System.out.println("Why'd you run the program..."
+            + "Removing Operating System");
+        }
+    }
 }
